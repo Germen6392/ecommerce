d48a980efb31ca0e2bb1adbffb3019274a74a122
Germen6392 <gerbarinas25@gmail.com>
2023-11-07 09:40:36 -0300

    react app added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Product from './assets/components/Product'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container'>
+        <h1>Detalle del Producto</h1>
+          <Product/>
+        
+      </div>  
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Product.jsx
@@ -0,0 +1,19 @@
+import './Product.css'
+function Product() {
+    return ( 
+    <>
+        <div className="container-product">
+        <article>
+          <h3>Notebook Dell Inspiron</h3>
+            <ul>
+              <li>SKU 3525</li>
+              <li>AMD Ryzen 5 5500U 8 GB de RAM 256 GB SSD</li>
+              <li>$479.999</li>
+              <li>Cantidad disponible 10</li>
+            </ul>  
+        </article>
+        </div>
+    </>
+    )
+}
+export default Product
