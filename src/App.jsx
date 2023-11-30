184fd2ee3cd6b5cf01ec1524d47fbf8c5aa93fd0
Germen6392 <gerbarinas25@gmail.com>
2023-11-30 09:39:44 -0300

    display button centre

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,21 @@ function App() {
   return (
     <>
 
-    <main>
+      <main>
     
-      <div className='container'>
-        <h1>Detalle del Producto</h1>
-          <Product
+        <div className='container'>
+          <h1>Detalle del Producto</h1>
+            <Product
                 name="Notebook Dell Inspiron"
                 sku="3525"
                 description="AMD Ryzen 5 5500U 8 GB de RAM 256 GB SSD"
                 price="$479.999"
                 quantity="10"/>
 
-      </div>
-    </main>
-      <Button/>
-
-      
+        </div>
+        <Button/>
+      </main>
+    
     </>
   )
 }
